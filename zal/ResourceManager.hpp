2835e6cbe79dbeeb4869b35a2bc8d37c345a34d4
paulinaczy <73669289+paulinaczy@users.noreply.github.com>
2020-11-11 23:14:27 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,5 +4,23 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+Resource* R;
+public:
+ResourceManager(){
+R = new Resource;}    
+
+ResourceManager(const ResourceManager &RM){
+R = new Resource{ *RM.R };}
+
+ResourceManager operator=(const ResourceManager& RM){
+if (&RM != this) {
+delete R;
+R = new Resource{ *RM.R };}
+return *this;}
+
+~ResourceManager() {
+delete R;}
+
+double get(){
+return R->get();}    
 };
