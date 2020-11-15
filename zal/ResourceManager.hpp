cef0a4e0c32d1226ec755614b3d8cda5f7b88595
paulinaczy <73669289+paulinaczy@users.noreply.github.com>
2020-11-15 19:11:48 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -21,6 +21,18 @@ return *this;}
 ~ResourceManager() {
 delete R;}
 
+ResourceManager(ResourceManager&& RM){ 
+ R = nullptr;
+ R = RM.R;
+ RM.R = nullptr;}
+  
+ ResourceManager operator=(ResourceManager&& RM){
+ if (&RM != this) {
+ delete R;
+ R = RM.R;
+ RM.R = nullptr;}
+ return *this;}
+  
 double get(){
-return R->get();}    
+return R->get();}  
 };
